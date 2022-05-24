a424f179986ef0971b9c5ddb80e390ee5013cb92
Guilherme Pontes <guilherme.bacelar@ucsal.edu.br>
2022-05-24 15:42:59 -0300

    First commit

---
+++ backend/src/main/java/com/guilherme/descatalog/DescatalogApplication.java
@@ -0,0 +1,12 @@
+package com.guilherme.descatalog;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DescatalogApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/guilherme/descatalog/DescatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.descatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
